8094a973ac02d8fe57566849773c771594ae4752
Marcosotola <marcosotola@gmail.com>
2024-02-16 20:55:32 -0300

    estilizando home

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -11,6 +11,18 @@ const Home = () => {
       <div className='slider-container'>
         <Slider />
       </div>
+      <div className='textHome'>
+        <p className='text-home'>
+          Contamos con un </p>
+        <strong className='bold'>
+          equipo profesional altamente capacitado
+        </strong>
+        <p className="text-home">y especializado en tratamientos estéticos y dermatológicos. Brindamos un servicio personalizado para cada uno de nuestros pacientes.
+        </p>
+      </div>
+      <h1 className='sub-title'>
+        <span  className='subtitle'>Nuestras Promos</span>
+      </h1>
     </>
   )
 }
