3f2247e97a853df59282644bbe0789ff3572c18a
Marcosotola <marcosotola@gmail.com>
2024-02-18 21:08:50 -0300

    deploy

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Slider from '../../components/carousel/carousel/Slider'
+import Promos from './components/Promos'
 import './Home.css'
 
 const Home = () => {
@@ -23,6 +24,8 @@ const Home = () => {
       <h1 className='sub-title'>
         <span  className='subtitle'>Nuestras Promos</span>
       </h1>
+      <Promos />
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur laudantium reprehenderit beatae consectetur ut obcaecati voluptates voluptatum ea ipsum impedit! Maiores, architecto. Nam iure ipsum quia assumenda repellendus, itaque ex!Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem libero corrupti corporis labore voluptatum doloremque neque cumque quisquam sequi quaerat optio, expedita nobis eius doloribus enim, ea officia numquam quibusdam!
     </>
   )
 }
